8bd90bfd5698fc5f19f89c766f3d19d8a90edaf0
evgimov <evgimov@gmail.com>
2016-12-02 19:45:25 -0800

    Added hasVotedFor function

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -4,14 +4,26 @@ export default React.createClass({
   getPair() {
     return this.props.pair || [];
   },
+  isDisabled() {
+    return !!this.props.hasVoted;
+  },
+  // label for voted
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  },
   render() {
     return <div className="voting">
       {this.getPair().map(entry =>
         <button key={entry}
-            onClick={() => this.props.vote(entry)}>
+                disabled={this.isDisabled()}
+                onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+          <div className="label">Voted</div> : null}
         </button>
       )}
     </div>;
   }
+
+
 });
