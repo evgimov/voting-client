361c7a6f1c128bffab797097ab904067b48b69dc
evgimov <evgimov@gmail.com>
2016-12-08 12:19:40 -0800

    Added winner component to render

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 const Results = React.createClass({
   mixins: [PureRenderMixin],
@@ -15,25 +16,27 @@ const Results = React.createClass({
   },
 
   render() {
-    return <div className="results">
-      <div className="tally">
-        {this.getPair().map(entry =>
-          <div key={entry} className="entry">
-            <h1>{entry}</h1>
-            <div className="voteCount">
-              {this.getVotes(entry)}
+    return this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
       </div>
-      <div className="management">
-        <button ref="next"
-                className="next"
-                onClick={this.props.next}>
-          Next
-        </button>
-      </div>
-    </div>;
   }
 });
 
