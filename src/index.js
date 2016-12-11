5fc7e10c37b270ba737b50400c1872ad5f1e10a0
evgimov <evgimov@gmail.com>
2016-12-10 17:22:18 -0800

    Added socket.io

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {VotingContainer} from './components/Voting';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {ResultsContainer} from './components/Results';
@@ -20,6 +21,7 @@ store.dispatch({
   }
 });
 
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
