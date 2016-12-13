993a3964577d8f024b87b3fb31c14178224237c5
evgimov <evgimov@gmail.com>
2016-12-13 13:58:03 -0800

    Added meta property to vote function

--- src/index.js
+++ src/index.js
@@ -11,17 +11,20 @@ import App from './components/App';
 import {ResultsContainer} from './components/Results';
 import {setState} from './action_creators';
 
-const createStoreWithMiddleware = applyMiddleware(
-  remoteActionMiddleware
-)(createStore);
-
-const store = createStoreWithMiddleware(reducer);
-
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+
+
 
 
 
